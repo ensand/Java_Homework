6abe165528b661e01cabd7dbc69b2a9992315d8e
ensand <enola1998@gmail.com>
2018-05-10 20:40:22 +0300

    Complete

--- Harjutus_1/src/Duplicates.java
+++ Harjutus_1/src/Duplicates.java
@@ -1,29 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 //Ülesanne 7
 public class Duplicates {
 
-    public static int duplicateCount(int[] numList, int num){
+    public static int duplicateCount(ArrayList<Integer> numList, int num){
         int count = 0;
         for (int element:numList) {
             if (element == num){
                 count = 1 + count;
+                //System.out.println("Boop!");
             }
         }
         return count;
     }
 
-    public static int duplicateSum(int[] numList){
+    public static int duplicateSum(ArrayList<Integer> numList){
+        int count = 0;
         int sum = 0;
-        for (int i = 0; i < numList.length; i ++){
-            if (numList[i] == numList[i+1]){
-                sum = numList[i] + sum;
+        for (int i = 0; i < numList.size(); i ++) {
+            count = Collections.frequency(numList, numList.get(i));
+            if (count == 1) {
+                numList.remove(i);
             }
+        }
+        for (int num : numList) {
+            sum += num;
+            //System.out.println("sum: " + sum);
         }
         return sum;
     }
 
-//    public static void main(String[] args) {
-//        duplicateCount(args[0], args[1]);
-//        duplicateSum(args[0]);
-//    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numList = new ArrayList<>();
+        numList.addAll(Arrays.asList(1, 2, 3, 2, 1));
+
+        System.out.println(duplicateCount(numList, 1));
+        System.out.println(duplicateSum(numList));
+
+    }
 
 }
--- Harjutus_2/src/Replacer.java
+++ Harjutus_2/src/Replacer.java
@@ -1,36 +1,67 @@
-//import java.io.FileWriter;
-//import java.io.IOException;
-//import java.nio.file.Files;
-//import java.nio.file.Paths;
-//import java.util.stream.Stream;
-//
-//public class Replacer {
-//
-//    public static void replacer(String fileName, String replaceable, String replacer){
-//        try {
-//            FileWriter fw = new FileWriter(fileName);
-//            fw.write(fileName.replaceAll(replaceable, replacer));
-//            fw.flush();
-//            fw.close();
-//        } catch (IOException e) {
-//            System.out.println("no >:(");
-//            e.printStackTrace();
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Replacer {
+
+
+    public static ArrayList<String> readFile(String fileName)
+            throws IOException {
+
+        String line = null;
+        ArrayList<String> text = new ArrayList<>();
+
+        try{
+            FileReader fr = new FileReader(fileName);
+            BufferedReader br = new BufferedReader(fr);
+
+            while ((line = br.readLine()) != null) {
+                text.addAll(Arrays.asList(line));
+            }
+
+            br.close();
+
+        } catch (FileNotFoundException ex){
+            System.out.println("Unable to find file " + fileName);
+            ex.printStackTrace();
+        }
+
+//        for (String textLine : text) {
+//            System.out.println(textLine);
 //        }
-//    }
-//
-//    public static int readFile(String fileName, String replacer){
-//        try (Stream<String> stream = Files.lines(Paths.get(fileName))){
-//            stream.forEach(System.out::println);
-//        } catch (IOException e){
-//            e.printStackTrace();
-//        }
-////        int count = 0;
-////        for (int i = 0; i < fileName.length(); i++){
-////            if ([i] == replacer) {
-////                count = count + 1;
-////            }
-////        }
-////        return count;
-//    }
-//
-//}
+
+        return text;
+
+    }
+
+
+    public static void replacer(ArrayList<String> text, String replaceable, String replacer, String fileName) {
+
+//            line = line.replaceAll(replaceable, replacer);
+
+            try {
+                FileWriter fw = new FileWriter(fileName);
+                BufferedWriter writer = new BufferedWriter(fw);
+
+                for (String line : text) {
+                    writer.write(line.replaceAll(replaceable, replacer));
+                    writer.newLine();
+                }
+
+                writer.flush();
+                writer.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+            //System.out.println(line);
+
+    }
+
+
+    public static void main(String[] args) throws IOException{
+
+        replacer(readFile("text.txt"), "wand", "penis", "text.txt");
+
+    }
+}
